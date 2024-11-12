d52519dc341a3ed5d8703ee78dfcaeb00b1d2ad6
DIVI MANI SANKAR <manisankar.divi@gmail.com>
2024-11-12 16:29:48 +0530

    Update main.go

--- go-project/main.go
+++ go-project/main.go
@@ -16,7 +16,7 @@ func init() {
   // Can be any io.Writer, see below for File example
   log.SetOutput(os.Stdout)
 
-  // Only log the warning severity or above.
+  // Only log the warning severity or above. this 
   log.SetLevel(log.WarnLevel)
 }
 
