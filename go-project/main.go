575c1da9a6765d1fc3527868cbc6e8739c55ba5c
DIVI MANI SANKAR <manisankar.divi@gmail.com>
2025-03-18 18:42:54 +0530

    Update main.go

--- go-project/main.go
+++ go-project/main.go
@@ -4,39 +4,47 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func init() {
-  // Log as JSON instead of the default ASCII formatter
-  log.SetFormatter(&log.JSONFormatter{})
+	// Log as JSON instead of the default ASCII formatter
+	log.SetFormatter(&log.JSONFormatter{})
 
-  // Output to stdout instead of the default stderr
-  // Can be any io.Writer, see below for File example
-  log.SetOutput(os.Stdout)
+	// Output to stdout instead of the default stderr
+	log.SetOutput(os.Stdout)
 
-  // Only log the warning severity or above. this 
-  log.SetLevel(log.WarnLevel)
+	// Only log warnings or above
+	log.SetLevel(log.WarnLevel)
 }
 
-// hello world from 1
 // helloHandler handles HTTP requests to the root URL.
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	// This will return the string without.
 	fmt.Fprint(w, "Hello, World!") 
 }
 
 // main function sets up the HTTP server.
 func main() {
 	log.WithFields(log.Fields{
-    "animal": "walrus",
-    "size":   10,
-  }).Info("A group of walrus emerges from the ocean")
+		"animal": "walrus",
+		"size":   10,
+	}).Info("A group of walrus emerges from the ocean")
 
 	http.HandleFunc("/", helloHandler)
 	fmt.Println("Server is running on http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-    		log.Fatalf("Failed to start server: %v", err)
+
+	// Use http.Server with timeouts
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      nil,
+		ReadTimeout:  5 * time.Second,  // Prevent slow-read DoS
+		WriteTimeout: 10 * time.Second, // Prevent slow-write DoS
+		IdleTimeout:  15 * time.Second, // Limits idle connection reuse
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
 	}
 }
