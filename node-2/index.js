5c8318c5bde325285e32327fb8c2aaaf3137b9e5
root <root@ip-172-31-2-197.ap-south-1.compute.internal>
2025-03-18 05:47:32 +0000

    Added node-2

---
+++ node-2/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+// hello from soubOS!!
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Set port to 3000 or use PORT env variable
+
+app.get('/', (req, res) => {
+  res.send("Hello, GitHub Actions CI!");
+});
+
+app.listen(port, () => {
+  console.log(`Starting server on port ${port}...`);
+});
